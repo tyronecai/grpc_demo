625d2b54a3c597990fb808b0bfe587cea7e0ffcc
cai.rong <cai.rong@yottabyte.cn>
2023-02-23 09:40:06 +0800

    feat: add 《聊一聊 gRPC 中的拦截器》 https://my.oschina.net/lenve/blog/7819695

---
+++ grpc_server/src/main/java/org/example/BookServiceCall.java
@@ -0,0 +1,49 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.example;
+
+import io.grpc.ForwardingServerCall;
+import io.grpc.MethodDescriptor;
+import io.grpc.ServerCall;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// 代码来源 《聊一聊 gRPC 中的拦截器》 https://my.oschina.net/lenve/blog/7819695
+public class BookServiceCall<Req, Rsp>
+        extends ForwardingServerCall.SimpleForwardingServerCall<Req, Rsp> {
+    private static final Logger LOG = LoggerFactory.getLogger(BookServiceCall.class);
+    protected BookServiceCall(ServerCall<Req, Rsp> delegate) {
+        super(delegate);
+    }
+
+    @Override
+    public ServerCall<Req, Rsp> delegate() {
+        return super.delegate();
+    }
+
+    @Override
+    public MethodDescriptor<Req, Rsp> getMethodDescriptor() {
+        return super.getMethodDescriptor();
+    }
+
+    @Override
+    public void sendMessage(Rsp message) {
+        LOG.info("这是服务端返回给客户端的消息：{}", message);
+        super.sendMessage(message);
+    }
+}
---
+++ grpc_server/src/main/java/org/example/BookServiceCallListener.java
@@ -0,0 +1,45 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.example;
+
+import io.grpc.ForwardingServerCallListener;
+import io.grpc.ServerCall;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// 代码来源 《聊一聊 gRPC 中的拦截器》 https://my.oschina.net/lenve/blog/7819695
+public class BookServiceCallListener<Req> extends ForwardingServerCallListener<Req> {
+    private static final Logger LOG = LoggerFactory.getLogger(BookServiceCallListener.class);
+
+    private final ServerCall.Listener<Req> delegate;
+
+    public BookServiceCallListener(ServerCall.Listener<Req> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public ServerCall.Listener<Req> delegate() {
+        return delegate;
+    }
+
+    @Override
+    public void onMessage(Req message) {
+        LOG.info("这是客户端发来的消息，可以在这里进行预处理：{}", message);
+        super.onMessage(message);
+    }
+}
